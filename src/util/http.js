222d3846b3380b153177505e6295c2b42746323f
mmm-maymyatmon <maymyat.dev@gmail.com>
2025-03-03 17:10:59 +0700

    ✨ Edit event
    Edit event

--- src/util/http.js
+++ src/util/http.js
@@ -24,6 +24,7 @@ export async function fetchEvents({ signal, searchTerm }) {
   return events;
 }
 
+
 export async function createNewEvent(eventData) {
   const response = await fetch(`http://localhost:3000/events`, {
     method: 'POST',
@@ -83,6 +84,25 @@ export async function deleteEvent({ id }) {
 
   if (!response.ok) {
     const error = new Error('An error occurred while deleting the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return response.json();
+}
+
+export async function updateEvent({ id, event }) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ event }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while updating the event');
     error.code = response.status;
     error.info = await response.json();
     throw error;
